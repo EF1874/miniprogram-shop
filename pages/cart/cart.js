ae9ecb3f1d8ff8bf57bf2a8940e4bd86aabfc113
EF_Lee <leason>
2020-07-01 19:41:46 +0800

    feat: 创建购物车页面，实现获取收货地址按钮

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,32 @@
 // pages/cart/cart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取收货地址
+  chooseAddress() {
+    // 先获取授权状态
+    wx.getSetting({
+      success: (res1) => {
+        console.log(res1)
+        // 如果状态为已经授权过或者是第一次点击授权，则直接调用获取地址API
+        const scopeAddress = res1.authSetting["scope.address"]
+        if (scopeAddress === true || scopeAddress === undefined) {
+          wx.chooseAddress({
+            success: (res2) => {
+              console.log(res2)
+            }
+          });
+        } else {
+          // 如果用户不小心点击了取消授权，则打开重新获取授权界面，授权完毕后再调用获取地址API
+          wx.openSetting({
+            success: (res3) => {
+              wx.chooseAddress({
+                success: (res2) => {
+                  console.log(res2)
+                }
+              });
+            }
+          });
+        }
+      }
+    });
   }
 })
