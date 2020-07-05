5ccff5122e2e3802c52933033672bf0508cb2ecb
EF_Lee <leason>
2020-07-05 21:59:20 +0800

    feat: 完成待支付商品页面，正在实现点击支付页面获取用户token

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -1,11 +1,11 @@
-// pages/pay/pay.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: [],
+    allChecked: false,
+    totalPrice: 0,
+    totalNum: 0
   },
 
   /**
@@ -16,51 +16,51 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+ * 生命周期函数--监听页面显示
+ */
   onShow: function () {
+    // 获取缓存的地址
+    const address = wx.getStorageSync("address");
+    // 获取缓存的购物车数据
+    let cart = wx.getStorageSync("cart") || [];
+    // 过滤后的购物车数据
+    cart = cart.filter(v => v.checked)
+    this.setData({
+      address
+    })
 
+    // 定义总金额和总数量
+    let totalNum = 0;
+    let totalPrice = 0;
+    // 遍历购物车数组，计算总金额和数量
+    cart.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.goods_price * v.num;
+        totalNum += v.num
+      }
+    })
+    // 将数据保存到data
+    this.setData({
+      cart,
+      totalNum,
+      totalPrice
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击支付事件
+  handleOrderPay() {
+    const { token } = wx.getStorageSync("token");
+    if (!token) {
+      wx.navigateTo({
+        url: '/pages/auth/auth',
+        success: (result)=>{
+          console.log(result)
+        },
+        fail: ()=>{},
+        complete: ()=>{}
+      });
+    } else {
+      console.log("已获取token")
+    }
   }
 })
